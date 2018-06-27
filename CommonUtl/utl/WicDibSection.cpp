274ea6f2cb344293c9a3bf174ac74c8e6f3c4377
Paul Cocoveanu T440p <phc.2nd@gmail.com>
2018-06-27 09:25:46 +0200

    enhance CWicDibSection: draw transparently images with alpha channel automatically, otherwise draw opaque

--- CommonUtl/utl/WicDibSection.cpp
+++ CommonUtl/utl/WicDibSection.cpp
@@ -37,41 +37,56 @@ CRect CWicDibSection::Draw( CDC* pDC, const CRect& boundsRect, ui::StretchMode s
 	if ( !IsValid() )
 		return boundsRect;
 
-	CDC memDC;
-	if ( NULL == pSrcDC && memDC.CreateCompatibleDC( pDC ) )
-		pSrcDC = &memDC;
-
-	const CSize& bmpSize = GetBmpFmt().m_size;
-	CRect imageRect( CPoint( 0, 0 ), ui::StretchSize( boundsRect.Size(), bmpSize, stretchMode ) );
+	const CRect bmpRect( CPoint( 0, 0 ), GetBmpFmt().m_size );
+	CRect imageRect( CPoint( 0, 0 ), ui::StretchSize( boundsRect.Size(), bmpRect.Size(), stretchMode ) );
 	ui::CenterRect( imageRect, boundsRect );
 
-	if ( pSrcDC != NULL )
-	{
-		CScopedGdiObj scopedBitmap( pSrcDC, GetSafeHandle() );
-
-		if ( bmpSize == imageRect.Size() )
-			pDC->BitBlt( imageRect.left, imageRect.top, imageRect.Width(), imageRect.Height(), pSrcDC, 0, 0, rop );
-		else
-		{
-			int oldStretchMode = pDC->SetStretchBltMode( COLORONCOLOR );
-			pDC->StretchBlt( imageRect.left, imageRect.top, imageRect.Width(), imageRect.Height(), pSrcDC, 0, 0, bmpSize.cx, bmpSize.cy, rop );
-			pDC->SetStretchBltMode( oldStretchMode );
-		}
-	}
+	BlitNatural( pDC, imageRect, bmpRect, pSrcDC, rop );
 	return imageRect;
 }
 
 CRect CWicDibSection::DrawAtPos( CDC* pDC, const CPoint& pos, CDC* pSrcDC /*= NULL*/ )
 {
+	const CRect bmpRect( CPoint( 0, 0 ), GetBmpFmt().m_size );
+	CRect imageRect( pos, bmpRect.Size() );
+
+	BlitNatural( pDC, imageRect, bmpRect, pSrcDC, SRCCOPY );
+	return imageRect;
+}
+
+bool CWicDibSection::BlitNatural( CDC* pDC, const CRect& destRect, const CRect& srcRect, CDC* pSrcDC /*= NULL*/, DWORD rop /*= SRCCOPY*/ ) const
+{
+	if ( !IsValid() )
+		return false;
+
 	CDC memDC;
 	if ( NULL == pSrcDC && memDC.CreateCompatibleDC( pDC ) )
 		pSrcDC = &memDC;
 
-	CRect imageRect( pos, GetBmpFmt().m_size );
-	if ( pSrcDC != NULL )
-	{
-		CScopedGdiObj scopedBitmap( pSrcDC, GetSafeHandle() );
-		pDC->BitBlt( imageRect.left, imageRect.top, imageRect.Width(), imageRect.Height(), pSrcDC, 0, 0, SRCCOPY );
+	if ( NULL == pSrcDC )
+		return false;
+
+	CScopedGdiObj scopedBitmap( pSrcDC, GetSafeHandle() );
+	int oldStretchMode = pDC->SetStretchBltMode( COLORONCOLOR );
+	bool succeeded = false;
+
+	if ( GetBmpFmt().m_hasAlphaChannel && SRCCOPY == rop )		// alpha channel present?
+	{	// draw transparently
+		BLENDFUNCTION blendFunc;
+		blendFunc.BlendOp = AC_SRC_OVER;						// use pre-multiplied alpha
+		blendFunc.BlendFlags = 0;
+		blendFunc.SourceConstantAlpha = 255;					// use per-pixel alpha values
+		blendFunc.AlphaFormat = AC_SRC_ALPHA;
+
+		succeeded = pDC->AlphaBlend( destRect.left, destRect.top, destRect.Width(), destRect.Height(), pSrcDC, srcRect.left, srcRect.top, srcRect.Width(), srcRect.Height(), blendFunc ) != FALSE;
 	}
-	return imageRect;
+
+	if ( !succeeded )
+		if ( destRect.Size() == srcRect.Size() )
+			succeeded = pDC->BitBlt( destRect.left, destRect.top, destRect.Width(), destRect.Height(), pSrcDC, 0, 0, rop ) != FALSE;
+		else
+			succeeded = pDC->StretchBlt( destRect.left, destRect.top, destRect.Width(), destRect.Height(), pSrcDC, 0, 0, srcRect.Width(), srcRect.Height(), rop ) != FALSE;
+
+	pDC->SetStretchBltMode( oldStretchMode );
+	return succeeded;
 }
--- CommonUtl/utl/WicDibSection.h
+++ CommonUtl/utl/WicDibSection.h
@@ -6,7 +6,8 @@
 #include "ui_fwd.h"
 
 
-// a DIB section generated from a WIC bitmap
+// a DIB section generated from a WIC bitmap;
+// it supports transparent drawing automatically if alpha channel is present.
 
 class CWicDibSection : public CBitmap, public CWicBitmap
 {
@@ -20,9 +21,11 @@ public:
 
 	HBITMAP CloneBitmap( void ) const;
 
-	// GDI drawing
-	CRect Draw( CDC* pDC, const CRect& boundsRect, ui::StretchMode stretchMode = ui::OriginalSize, CDC* pSrcDC = NULL, DWORD rop = SRCCOPY );			// returns the image rect
+	// GDI drawing (draws transparently if bitmap has alpha channel and rop is SRCCOPY)
+	CRect Draw( CDC* pDC, const CRect& boundsRect, ui::StretchMode stretchMode = ui::OriginalSize, CDC* pSrcDC = NULL, DWORD rop = SRCCOPY );		// returns the image rect
 	CRect DrawAtPos( CDC* pDC, const CPoint& pos, CDC* pSrcDC = NULL );
+protected:
+	bool BlitNatural( CDC* pDC, const CRect& destRect, const CRect& srcRect, CDC* pSrcDC = NULL, DWORD rop = SRCCOPY ) const;		// alpha blends (transparently) if alpha channel present, otherwise it blits.
 };
 
 
