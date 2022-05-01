58ffb56430c60494b05a7ba61417f2d35cbc93b9
Paul Cocoveanu T440p <phc.2nd@gmail.com>
2022-05-01 19:03:32 +0200

    add untracked files

---
+++ cmd_line/TreePlus/Table.cpp
@@ -0,0 +1,143 @@
+
+#include "stdafx.h"
+#include "Table.h"
+#include "utl/ContainerUtilities.h"
+#include "utl/TextFileIo.h"
+#include <hash_set>
+#include <deque>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#include "utl/TextFileIo.hxx"
+
+
+namespace hlp
+{
+	void PushFront( std::deque< TCHAR >& rOut, const std::tstring& text )
+	{
+		for ( std::tstring::const_reverse_iterator itChar = text.rbegin(); itChar != text.rend(); ++itChar )
+			rOut.push_front( *itChar );
+	}
+}
+
+
+// CTextCell implementation
+
+TCHAR CTextCell::s_columnSep[] = _T("\t");
+
+CTextCell::CTextCell( const CTextCell* pParent, const std::tstring& name )
+	: m_name( name )
+	, m_pParent( pParent )
+{
+}
+
+CTextCell::~CTextCell()
+{
+	utl::ClearOwningContainer( m_children );
+}
+
+CTextCell* CTextCell::AddChild( const std::tstring& name )
+{
+	CTextCell* pChild = FindCell( name );
+
+	if ( NULL == pChild )
+		m_children.push_back( pChild = new CTextCell( this, name ) );
+
+	ENSURE( pChild != NULL );
+	return pChild;
+}
+
+CTextCell* CTextCell::FindCell( const std::tstring& name ) const
+{
+	for ( std::vector< CTextCell* >::const_iterator itChild = m_children.begin(); itChild != m_children.end(); ++itChild )
+		if ( name == (*itChild)->GetName() )
+			return *itChild;
+
+	return NULL;
+}
+
+std::tstring CTextCell::MakePath( const CTextCell* pRoot /*= NULL*/ ) const
+{
+	std::deque< TCHAR > path( m_name.begin(), m_name.end() );
+	static std::tstring s_strColumnSep = s_columnSep;
+
+	for ( const CTextCell* pParent = m_pParent; pParent != pRoot; pParent = pParent->GetParent() )
+	{
+		hlp::PushFront( path, s_strColumnSep );
+		hlp::PushFront( path, pParent->GetName() );
+	}
+
+	return std::tstring( path.begin(), path.end() );
+}
+
+CTextCell* CTextCell::DeepFindCell( const TCHAR* pCellPath ) const
+{
+	std::vector< std::tstring > cellNames;
+	str::Split( cellNames, pCellPath, s_columnSep );
+
+	CTextCell* pCell = const_cast<CTextCell*>( this );
+
+	for ( std::vector< std::tstring >::const_iterator itCellName = cellNames.begin(); itCellName != cellNames.end() && pCell != NULL; ++itCellName )
+		pCell = pCell->FindCell( *itCellName );
+
+	return pCell;
+}
+
+void CTextCell::QuerySubFolders( std::vector< CTextCell* >& rSubFolders ) const
+{
+	utl::QueryThat( rSubFolders, m_children, std::mem_fun( &CTextCell::IsFolder ) );
+}
+
+void CTextCell::QueryLeafs( std::vector< CTextCell* >& rLeafs ) const
+{
+	utl::QueryThat( rLeafs, m_children, std::mem_fun( &CTextCell::IsLeaf ) );
+}
+
+
+// CTable implementation
+
+CTable::CTable( void )
+	: m_root( NULL, _T("<root>") )
+{
+}
+
+CTable::~CTable()
+{
+}
+
+fs::Encoding CTable::ParseTextFile( const fs::CPath& textFilePath ) throws_( CRuntimeException )
+{
+	std::vector< std::tstring > rows;
+	fs::Encoding encoding = io::ReadLinesFromFile( rows, textFilePath );
+
+	ParseRows( rows );
+	return encoding;
+}
+
+void CTable::ParseRows( const std::vector< std::tstring >& rows )
+{
+	stdext::hash_set< std::tstring > uniqueRows;
+
+	for ( std::vector< std::tstring >::const_iterator itRow = rows.begin(); itRow != rows.end(); ++itRow )
+		if ( uniqueRows.insert( *itRow ).second )		// is row unique? - filter duplicate rows
+			ParseColumns( *itRow );
+}
+
+void CTable::ParseColumns( const std::tstring& row )
+{
+	if ( row.empty() )
+		return;				// ignore empty rows
+
+	std::vector< std::tstring > columns;
+	str::Split( columns, row.c_str(), CTextCell::s_columnSep );
+
+	CTextCell* pPathCell = &m_root;
+
+	for ( std::vector< std::tstring >::const_iterator itColumn = columns.begin(); itColumn != columns.end(); ++itColumn )
+		if ( !itColumn->empty() )
+			pPathCell = pPathCell->AddChild( *itColumn );
+		else
+			break;			// break at empty column, and stop parsing
+}
---
+++ cmd_line/TreePlus/Table.h
@@ -0,0 +1,60 @@
+#ifndef Table_h
+#define Table_h
+#pragma once
+
+#include "utl/Encoding.h"
+
+
+// composite text cell structured as a hierarchy of folders and leafs, to describe cell nodes in a table
+
+class CTextCell
+{
+public:
+	CTextCell( const CTextCell* pParent, const std::tstring& name );
+	~CTextCell();
+
+	bool IsRoot( void ) const { return NULL == m_pParent; }
+	bool IsFolder( void ) const { return !IsLeaf(); }
+	bool IsLeaf( void ) const { return m_children.empty(); }
+
+	const CTextCell* GetParent( void ) const { return m_pParent; }
+	CTextCell* AddChild( const std::tstring& name );
+
+	const std::tstring& GetName( void ) const { return m_name; }
+	CTextCell* FindCell( const std::tstring& name ) const;
+
+	std::tstring MakePath( const CTextCell* pRoot = NULL ) const;
+	CTextCell* DeepFindCell( const TCHAR* pCellPath ) const;		// path names are separated by s_columnSep ('\t')
+
+	const std::vector< CTextCell* >& GetChildren( void ) const { return m_children; }
+	void QuerySubFolders( std::vector< CTextCell* >& rSubFolders ) const;
+	void QueryLeafs( std::vector< CTextCell* >& rLeafs ) const;
+private:
+	std::tstring m_name;
+	const CTextCell* m_pParent;
+	std::vector< CTextCell* > m_children;
+public:
+	static TCHAR s_columnSep[];
+};
+
+
+// uses an input stream of rows of tab-separated values to build a hierarchy of folders and leafs cells
+
+class CTable
+{
+public:
+	CTable( void );
+	~CTable();
+
+	const CTextCell* GetRoot( void ) const { return &m_root; }
+
+	fs::Encoding ParseTextFile( const fs::CPath& textFilePath ) throws_( CRuntimeException );
+
+	void ParseRows( const std::vector< std::tstring >& rows );
+	void ParseColumns( const std::tstring& row );
+private:
+	CTextCell m_root;		// table root
+};
+
+
+#endif // Table_h
