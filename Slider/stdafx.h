969cd29d83ac4a75e3a8ad7b2793239c1ed0cd3f
Paul Cocoveanu T440p <phc.2nd@gmail.com>
2023-01-22 09:45:24 +0100

    rollback with changed capitalization

---
+++ Slider/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently,
+// but are changed infrequently
+
+#pragma once
+
+#include "utl/utl_base.h"
+
+#include <afxwin.h>					// MFC core and standard components
+#include <afxext.h>         		// MFC extensions
+#include <afxdisp.h>        		// MFC Automation classes
+#include <afxdtctl.h>				// MFC support for Internet Explorer 4 Common Controls
+#include <afxcmn.h>					// MFC support for Windows Common Controls
+
+#include <afxmt.h>
+#include <comdef.h>
+#include <atlbase.h>				// For CComPtr<> and stuff
+
+#include "utl/UI/utl_ui.h"
+
+
+#ifdef _DEBUG
+	#define USE_UT		// no UT code in release builds
+#endif // _DEBUG
+
+
+#define PROF_SEP _T("|")
