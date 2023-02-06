183aae324ab4daea2264f83cb28003cbb724c63f
Paul Cocoveanu T440p <phc.2nd@gmail.com>
2023-02-06 14:24:51 +0100

    extract str::CSeparatorsPair from str::CEnclosedParser

--- CommonUtl/utl/StringParsing.h
+++ CommonUtl/utl/StringParsing.h
@@ -15,37 +15,133 @@ namespace code
 
 namespace str
 {
-	// A set of START and END separators to search for enclosed identifiers, in forward direction, by position.
-	// Provides parsing a string for enclosed items delimited by any of the multiple START/END separator pairs (e.g. environment variables).
+	// A set of START and END separators to search for enclosed sequences.
 	//
 	template< typename CharT >
-	class CEnclosedParser : private utl::noncopyable
+	class CSeparatorsPair : private utl::noncopyable
 	{
 	public:
 		typedef std::basic_string<CharT> TString;
 		typedef std::vector<TString> TSepVector;
 		typedef size_t TSepMatchPos;						// position in TSepVector of the matching START and END separators
-		typedef std::pair<size_t, size_t> TIdentSpecPair;	// START and END positions of a full identifier spec "<startSep>identifier<endSep>"
 
-		CEnclosedParser( const CharT* pStartSepList, const CharT* pEndSepList, bool matchIdentifier = true, const CharT listDelim[] = nullptr /* "|" */ )
-			: m_matchIdentifier( matchIdentifier )
+		CSeparatorsPair( const CharT* pStartSepList, const CharT* pEndSepList, const CharT listDelim[] = nullptr /* "|" */ )
 		{
-			InitSeparators( pStartSepList, pEndSepList, listDelim );
-		}
+			const CharT defaultDelim[] = { '|', '\0' };		// "|"
+			if ( nullptr == listDelim )
+				listDelim = defaultDelim;
 
-		CEnclosedParser( bool matchIdentifier, const char* pStartSepList, const char* pEndSepList, const char listDelim[] = "|" )		// narrow seps constructor
-			: m_matchIdentifier( matchIdentifier )
-		{
-			InitSeparators( str::ValueToString<TString>( pStartSepList ).c_str(), str::ValueToString<TString>( pEndSepList ).c_str(), listDelim );
+			str::Split( m_sepsPair.first, pStartSepList, listDelim );
+			str::Split( m_sepsPair.second, pEndSepList, listDelim );
+			StoreLeadingChars();
+
+			ENSURE( !m_sepsPair.first.empty() && m_sepsPair.first.size() == m_sepsPair.second.size() );
+			ENSURE( !m_leadingChars.empty() );
 		}
 
 		bool IsValid( void ) const { return !m_sepsPair.first.empty(); }
 		const std::pair<TSepVector, TSepVector>& GetSepsPair( void ) const { return m_sepsPair; }
 
 		bool IsValidMatchPos( TSepMatchPos sepMatchPos ) const { return sepMatchPos < m_sepsPair.first.size(); }
+
+		size_t FindAnyStartSep( TSepMatchPos* pOutSepMatchPos, const TString& text, size_t offset = 0 ) const
+		{
+			ASSERT_PTR( pOutSepMatchPos );
+
+			for ( size_t startSepPos = offset; ( startSepPos = text.find_first_of( m_leadingChars, startSepPos ) ) != TString::npos; ++startSepPos )
+			{
+				TSepMatchPos sepMatchPos = FindSepMatchPos( text, startSepPos, m_sepsPair.first );
+
+				if ( sepMatchPos != utl::npos )
+				{
+					utl::AssignPtr( pOutSepMatchPos, sepMatchPos );
+					return startSepPos;
+				}
+			}
+
+			return TString::npos;
+		}
+
+		bool MatchesAnyStartSepAt( TSepMatchPos* pOutSepMatchPos, const TString& text, size_t offset ) const	// use this in outside loops
+		{
+			REQUIRE( offset != text.length() );
+
+			if ( m_leadingChars.find( text[ offset ] ) != TString::npos )		// quick match of any separator leading character?
+			{
+				TSepMatchPos sepMatchPos = FindSepMatchPos( text, offset, m_sepsPair.first );
+
+				if ( sepMatchPos != utl::npos )
+				{
+					utl::AssignPtr( pOutSepMatchPos, sepMatchPos );
+					return true;
+				}
+			}
+
+			return false;
+		}
+	private:
+		static TSepMatchPos FindSepMatchPos( const TString& text, size_t offset, const TSepVector& separators )
+		{
+			for ( TSepMatchPos i = 0; i != separators.size(); ++i )
+			{
+				const TString& startSep = separators[ i ];
+
+				if ( str::EqualsAt( text, offset, startSep ) )
+					return i;		// found matching separator position
+			}
+
+			return utl::npos;
+		}
+	private:
+		void StoreLeadingChars( void )
+		{
+			for ( size_t i = 0; i != m_sepsPair.first.size(); ++i )
+			{
+				REQUIRE( !m_sepsPair.first[i].empty() && !m_sepsPair.second[i].empty() );		// START and END separators must be valid
+
+				CharT leadingCh = m_sepsPair.first[ i ][ 0 ];
+
+				if ( TString::npos == m_leadingChars.find( leadingCh ) )		// is it unique?
+					m_leadingChars.push_back( leadingCh );
+			}
+		}
+	private:
+		std::pair<TSepVector, TSepVector> m_sepsPair;	// <START seps, END seps>
+		TString m_leadingChars;							// for quick matching of the first letter when searching for START separators
+	};
+
+
+	// Provides parsing a string for enclosed items delimited by any of the multiple START/END separator pairs (e.g. environment variables).
+	// For parsing in forward direction, by position.
+	//
+	template< typename CharT >
+	class CEnclosedParser : private utl::noncopyable
+	{
+	public:
+		typedef std::basic_string<CharT> TString;
+		typedef CSeparatorsPair<CharT> TSeparatorsPair;
+		typedef typename TSeparatorsPair::TSepMatchPos TSepMatchPos;	// position in TSepVector of the matching START and END separators
+		typedef std::pair<size_t, size_t> TIdentSpecPair;	// START and END positions of a full identifier spec "<startSep>identifier<endSep>"
+
+		CEnclosedParser( const CharT* pStartSepList, const CharT* pEndSepList, bool matchIdentifier = true, const CharT listDelim[] = nullptr /* "|" */ )
+			: m_matchIdentifier( matchIdentifier )
+			, m_seps( pStartSepList, pEndSepList, listDelim )
+		{
+		}
+
+		CEnclosedParser( bool matchIdentifier, const char* pStartSepList, const char* pEndSepList, const char listDelim[] = "|" )
+			: m_matchIdentifier( matchIdentifier )
+			, m_seps( str::ValueToString<TString>( pStartSepList ).c_str(), str::ValueToString<TString>( pEndSepList ).c_str(), listDelim )
+		{	// narrow seps constructor, works for both char/wchar_t parsers
+		}
+
+		bool IsValid( void ) const { return m_seps.IsValid(); }
+		const TSeparatorsPair& GetSeparators( void ) const { return m_seps; }
+
+		bool IsValidMatchPos( TSepMatchPos sepMatchPos ) const { return sepMatchPos < m_seps.GetSepsPair().first.size(); }
 		bool IsValidIdentifier( TSepMatchPos sepMatchPos, const TIdentSpecPair& specBounds ) const { return GetIdentLength( sepMatchPos, specBounds ) != 0; }
-		const TString& GetStartSep( TSepMatchPos sepMatchPos ) const { REQUIRE( IsValidMatchPos( sepMatchPos ) ); return m_sepsPair.first[ sepMatchPos ]; }
-		const TString& GetEndSep( TSepMatchPos sepMatchPos ) const { REQUIRE( IsValidMatchPos( sepMatchPos ) ); return m_sepsPair.second[ sepMatchPos ]; }
+		const TString& GetStartSep( TSepMatchPos sepMatchPos ) const { REQUIRE( IsValidMatchPos( sepMatchPos ) ); return m_seps.GetSepsPair().first[ sepMatchPos ]; }
+		const TString& GetEndSep( TSepMatchPos sepMatchPos ) const { REQUIRE( IsValidMatchPos( sepMatchPos ) ); return m_seps.GetSepsPair().second[ sepMatchPos ]; }
 
 		TString MakeSpec( const TIdentSpecPair& specBounds, const TString& text ) const
 		{
@@ -79,7 +175,7 @@ namespace str
 			ASSERT_PTR( pOutSepMatchPos );
 			REQUIRE( IsValid() && offset <= text.length() );
 
-			size_t startSepPos = FindAnyStartSep( pOutSepMatchPos, text, offset );
+			size_t startSepPos = m_seps.FindAnyStartSep( pOutSepMatchPos, text, offset );
 			size_t endPos = TString::npos;
 
 			if ( startSepPos != utl::npos )
@@ -140,67 +236,8 @@ namespace str
 			return itemCount;
 		}
 	private:
-		size_t FindAnyStartSep( TSepMatchPos* pOutSepMatchPos, const TString& text, size_t offset = 0 ) const
-		{
-			ASSERT_PTR( pOutSepMatchPos );
-
-			for ( size_t startSepPos = offset; ( startSepPos = text.find_first_of( m_leadingChars, startSepPos ) ) != TString::npos; ++startSepPos )
-			{
-				TSepMatchPos sepMatchPos = FindMatchPos( text, startSepPos );
-
-				if ( sepMatchPos != utl::npos )
-				{
-					utl::AssignPtr( pOutSepMatchPos, sepMatchPos );
-					return startSepPos;
-				}
-			}
-
-			return TString::npos;
-		}
-
-		TSepMatchPos FindMatchPos( const TString& text, size_t offset ) const
-		{
-			for ( TSepMatchPos i = 0; i != m_sepsPair.first.size(); ++i )
-			{
-				const TString& startSep = m_sepsPair.first[ i ];
-
-				if ( str::EqualsAt( text, offset, startSep ) )
-					return i;		// found matching separator position
-			}
-
-			return utl::npos;
-		}
-
-		void StoreLeadingChars( void )
-		{
-			for ( size_t i = 0; i != m_sepsPair.first.size(); ++i )
-			{
-				REQUIRE( !m_sepsPair.first[i].empty() && !m_sepsPair.second[i].empty() );		// START and END separators must be valid
-
-				CharT leadingCh = m_sepsPair.first[ i ][ 0 ];
-
-				if ( TString::npos == m_leadingChars.find( leadingCh ) )		// is it unique?
-					m_leadingChars.push_back( leadingCh );
-			}
-		}
-
-		void InitSeparators( const CharT* pStartSepList, const CharT* pEndSepList, const CharT listDelim[] )
-		{
-			const CharT defaultDelim[] = { '|', '\0' };		// "|"
-			if ( nullptr == listDelim )
-				listDelim = defaultDelim;
-
-			str::Split( m_sepsPair.first, pStartSepList, listDelim );
-			str::Split( m_sepsPair.second, pEndSepList, listDelim );
-			StoreLeadingChars();
-
-			ENSURE( !m_sepsPair.first.empty() && m_sepsPair.first.size() == m_sepsPair.second.size() );
-			ENSURE( !m_leadingChars.empty() );
-		}
-	private:
-		std::pair<TSepVector, TSepVector> m_sepsPair;	// <START seps, END seps>
-		bool m_matchIdentifier;							// if true matches only identifiers "VAR1", otherwise matches anything "VAR.2"
-		TString m_leadingChars;							// for quick matching of the first letter when searching for START separators
+		bool m_matchIdentifier;			// if true matches only identifiers "VAR1", otherwise matches anything "VAR.2"
+		TSeparatorsPair m_seps;			// <START seps, END seps>
 	};
 }
 
